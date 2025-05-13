49496e9f494cd4363c087f06c495ed683400aad8
Pavle Matijasevic <pavlematijasevic171790@gmail.com>
2025-05-13 09:58:29 +0200

    create login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,82 @@
-import { Button } from "@mui/material";
+"use client";
+import { Button, TextField } from "@mui/material";
 import Image from "next/image";
+import loginImage from "../../public/login.jpg";
+import { useState } from "react";
+import { checkEmail, checkPassword } from "../app/src/utiles/utiles";
 
 export default function Home() {
+  const year: number = new Date().getFullYear();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+
+  function handleLogin() {
+    setSubmitted(true);
+    const error = checkEmail(email);
+    setEmailError(error);
+    const pError = checkPassword(password);
+    setPasswordError(pError);
+    if (!error) {
+      // Nastavi sa slanjem forme
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="typography-h1 text-primary bg-red-500 ">Hello world</div>
-      <Button variant="contained" className="bg-blue">
-        Primary
-      </Button>
-      <Button disabled>Disabled</Button>
-      <Button href="#text-buttons">Link</Button>
+    <div
+      className="w-screen h-screen bg-cover bg-center flex items-center justify-center "
+      style={{ backgroundImage: `url(${loginImage.src})` }}
+    >
+      <div className="bg-loginbackground p-8 mx-auto w-[495px] rounded-md">
+        <h1 className="mb-4 text-mytext text-center font-semibold text-xl">
+          Online biblioteka
+        </h1>
+        <div className="bg-grey-line w-full h-[1px] "></div>
+        <form>
+          <TextField
+            className="mb-6 mt-6 w-full rounded-sm"
+            id="outlined-required"
+            label="Email"
+            placeholder="example@example.net"
+            onChange={(e) => {
+              setEmail(e.target.value);
+              const error = checkEmail(e.target.value);
+              setEmailError(error);
+            }}
+            error={submitted && !!emailError}
+            helperText={submitted && emailError ? emailError : ""}
+          />
+          <TextField
+            type="password"
+            className="mb-6 w-full rounded-sm"
+            id="outlined-required"
+            label="Password"
+            placeholder="Enter password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+              const error = checkPassword(e.target.value);
+              setPasswordError(error);
+            }}
+            error={submitted && !!passwordError}
+            helperText={submitted && passwordError ? passwordError : ""}
+          />
+          <Button
+            variant="contained"
+            className="bg-blue w-full mb-4 font-medium text-sm p-3"
+            onClick={handleLogin}
+          >
+            LOG IN
+          </Button>
+        </form>
+        <Button variant="text" className="text-blue p-2 capitalize mb-4 block">
+          Forgot password?
+        </Button>
+        <div className=" text-grey-text text-xs text-center">
+          {`©${year} ICT Cortex. All rights reserved.`}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/app/src/utiles/utiles.ts
@@ -0,0 +1,52 @@
+export function checkEmail(email: string): string | null {
+  
+  const errors = [];
+    if (email.length === 0) 
+        {
+            errors.push('Unijeli ste prazan email!!!');
+            return  `${errors}`
+        }
+    
+
+  const hasAtSymbol = email.includes("@");
+  const hasDotAfterAt = hasAtSymbol && email.split("@")[1]?.includes(".");
+  const validLocalPart = email.split("@")[0]?.length > 0;
+  const validDomainPart = hasDotAfterAt && email.split("@")[1].split(".")[0]?.length > 0;
+  const validTld = hasDotAfterAt && email.split("@")[1].split(".")[1]?.length > 0;
+
+  
+
+  if (!hasAtSymbol) errors.push("nedostaje znak '@'");
+  if (!validLocalPart) errors.push("nedostaje dio prije '@'");
+  if (!validDomainPart) errors.push("nedostaje dio između '@' i '.'");
+  if (!validTld) errors.push("nedostaje završetak poput '.com'");
+
+  return errors.length > 0 ? `Neispravan email: ${errors.join(", ")}.` : null;
+}
+
+
+export function checkPassword(password: string): string | null {
+  const errors: string[] = [];
+
+  if (password.length === 0) 
+        {
+            errors.push('Unijeli ste prazan password!!!');
+            return  `${errors}`
+        }
+
+  if (password.length < 8) {
+    errors.push("lozinka mora imati najmanje 8 karaktera");
+  }
+  if (!/[a-z]/.test(password)) {
+    errors.push("lozinka mora sadržati najmanje jedno malo slovo");
+  }
+  if (!/[A-Z]/.test(password)) {
+    errors.push("lozinka mora sadržati najmanje jedno veliko slovo");
+  }
+  if (!/[\W_]/.test(password)) {
+    errors.push("lozinka mora sadržati najmanje jedan specijalni karakter");
+  }
+
+  return errors.length > 0 ? `Neispravan password: ${errors.join(", ")}.` : null;
+}
+
